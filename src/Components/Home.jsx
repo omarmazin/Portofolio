6d6974a12919a2024e2b91083ccbfd98bc5185af
OmarMazin <77233657+omarmazin@users.noreply.github.com>
2023-07-30 05:52:57 +0300

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -12,12 +12,13 @@ const Home = () => {
       <div className=" max-w-screen-lg mx-auto flex flex-col md:flex-row items-center justify-center h-full px-4">
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-7xl font-bold text-white">
-            I'm a Full Stack Developer
+            I'm a Front End Developer
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
-            I have 1 year of experience building and desgining software.
-            Currently, I love to work on web application using technologies like
-            React, Tailwind, Typescript and Next.Js .
+            Detail-oriented Front End Developer with more than 2 years 
+            experience working with HTML, CSS, Javascript and React.js .
+            Highly adept at both independent and and collaborative projects, 
+            with an ephasis on landing page and website development .
           </p>
           <div>
             <Link
